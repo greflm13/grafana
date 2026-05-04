f915eec92f353deae2e04eb927068689b46aa93c
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2026-05-05 00:26:21 +0200

    yo yo oy

--- apache_vhosts.py
+++ apache_vhosts.py
@@ -1,21 +1,71 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import os
-import time
+import json
+import urllib3
 
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+import requests
 
-token = os.environ.get("INFLUXDB_TOKEN")
-org = "sorogon"
-bucket = "devices"
-url = "https://influxdb.sorogon.eu"
+from apacheconfig import make_loader
 
-client = InfluxDBClient(url=url, token=token, org=org)
+urllib3.disable_warnings()
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
 
-for value in range(5):
-    point = Point("apache_vhost").tag("tagname1", "tagvalue1").field("field1", value)
-    write_api.write(bucket=bucket, org="sorogon", record=point)
-    time.sleep(1)  # separate points by 1 second
+def get_vhosts(path: str = "/etc/apache2/sites-enabled") -> list[dict[str, str | None]]:
+    lsit = []
+    for file in os.listdir(path):
+        with make_loader() as loader:
+            data = loader.load(os.path.join(path, file))
+
+            assert isinstance(data, dict)
+            if "IfModule" in data:
+                vhosts = data["IfModule"]["mod_ssl.c"].get("VirtualHost", {})
+                for vhost in vhosts:
+                    lsit.append(
+                        {
+                            "url": vhost["*:443"].get("ServerName", None),
+                            "proxy": vhost["*:443"].get("ProxyPass", None),
+                        }
+                    )
+    return lsit
+
+
+def try_host(vhost: dict[str, str | None]) -> dict[str, str | int]:
+    ret = {}
+    assert isinstance(vhost["url"], str)
+    try:
+        req = requests.get("https://" + vhost["url"], verify=False, allow_redirects=True)
+        ret = {"url": vhost["url"], "status": req.status_code, "proxy": -1}
+    except requests.ConnectionError as e:
+        if e.response:
+            ret = {"url": vhost["url"], "status": e.response.status_code, "proxy": -1}
+        else:
+            ret = {"url": vhost["url"], "status": 600, "proxy": -1}
+    if "proxy" in vhost and vhost["proxy"] is not None:
+        try:
+            req = requests.get(vhost["proxy"].split()[1], verify=False, allow_redirects=True)
+            return {"url": ret["url"], "status": ret["status"], "proxy": req.status_code}
+        except requests.ConnectionError as e:
+            if e.response:
+                return {"url": ret["url"], "status": ret["status"], "proxy": e.response.status_code}
+            else:
+                return {"url": ret["url"], "status": ret["status"], "proxy": 600}
+    return ret
+
+
+def main() -> None:
+    metrics = []
+    vhosts = get_vhosts()
+    for vhost in vhosts:
+        host = try_host(vhost)
+        data = {
+            "vhost": str(host["url"]),
+            "status": int(host["status"]),
+            "proxy": int(host["proxy"]),
+        }
+        metrics.append(data)
+
+    print(json.dumps({"measurement": "vhosts", "data": metrics}))
+
+
+if __name__ == "__main__":
+    main()

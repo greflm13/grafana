5588fd28b17671b000bd8468cbfcc6b2beab99c8
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2026-05-11 10:10:01 +0200

    added additional fields

--- qbittorrent.py
+++ qbittorrent.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 import sys
 
 from time import sleep
@@ -6,12 +7,16 @@ from typing import NoReturn
 
 import qbittorrentapi
 
+shortner = re.compile(r"(.2160p.*|.1080p.*|.720p.*|.x265.*|.x264.*)")
+
+
+def shortify_name(name: str) -> str:
+    return shortner.sub("", name).strip().replace(" ", "\\ ")
+
 
 def main() -> NoReturn:
     while True:
-        client = qbittorrentapi.Client(
-            host="", port=0, username="", password="", VERIFY_WEBUI_CERTIFICATE=False
-        )
+        client = qbittorrentapi.Client(host="", port=0, username="", password="", VERIFY_WEBUI_CERTIFICATE=False)
         client.auth_log_in()
 
         torrentlist = client.torrents.info(sort="added_on")
@@ -22,6 +27,7 @@ def main() -> NoReturn:
         for torrent in torrentlist:
             print(
                 f"torrents,name={torrent.name.replace(' ', '\\ ')}"
+                + f",short_name={shortify_name(torrent.name)}"
                 + f",category={torrent.category.replace(' ', '\\ ')}"
                 + f",added_on={torrent.added_on}"
                 + f",hash={torrent.hash}"
@@ -29,6 +35,8 @@ def main() -> NoReturn:
                 + f" amount_left={torrent.amount_left}i"
                 + f",downloaded={torrent.downloaded}i"
                 + f",last_activity={torrent.last_activity}"
+                + f",num_complete={torrent.num_complete}i"
+                + f",num_incomplete={torrent.num_incomplete}i"
                 + f",num_leechs={torrent.num_leechs}i"
                 + f",num_seeds={torrent.num_seeds}i"
                 + f",popularity={torrent.popularity}"
